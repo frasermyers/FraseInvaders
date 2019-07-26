c12611c1f3e26f4b735ac7cbe646a9b934ee9d80
Fraser <frasermyersmusic@gmail.com>
2019-07-26 14:46:13 +1000

    Players on the field

--- Main.py
+++ Main.py
@@ -1,1 +1,133 @@
-Commit_3 = 0
+from tkinter import *
+import random
+import time
+
+tk = Tk()
+tk.title("Frase Invaders!")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=720, height=560, bd=0, highlightthickness=0)
+canvas.config(bg="black")
+canvas.pack()
+tk.update()
+
+
+class Sprite:
+
+    def __init__(self, canvas, color="white", x=10, y=10, w=15, h=15, x_vel=0, y_vel=0):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(x, y, x+w, y+h, fill=color)
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+
+    def draw(self):
+        self.canvas.move(self.id, self.x_vel, self.y_vel)
+
+    def hit_wall(self):
+        pos = self.canvas.coords(self.id)
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        if pos[2] >= self.canvas_width:
+            return True
+        if pos[0] <= 0:
+            return True
+        return False
+
+
+class Enemy(Sprite):
+
+    def __init__(self, canvas, color="Red", x=10, y=10, w=15, h=15, x_vel=1, y_vel=0):
+        super(Enemy, self).__init__(canvas, color=color , x=x, y=y, w=w, h=h, x_vel=x_vel, y_vel=y_vel)
+
+
+class Hero(Sprite):
+
+    def __init__(self, canvas, color="Blue", x=360, y=500, w=40, h=40, x_vel=0, y_vel=0):
+        super(Hero, self).__init__(canvas, color=color, x=x, y=y, w=w, h=h, x_vel=x_vel, y_vel=y_vel)
+        self.canvas.bind_all('<KeyPress-Left>', self.left_key_pressed)
+        self.canvas.bind_all('<KeyRelease-Left>', self.left_key_released)
+        self.canvas.bind_all('<KeyPress-Right>', self.right_key_pressed)
+        self.canvas.bind_all('<KeyRelease-Right>', self.right_key_released)
+
+    def left_key_pressed(self, evt):
+        self.x_vel = -2
+
+    def left_key_released(self, evt):
+        self.x_vel = 0
+
+    def right_key_pressed(self, evt):
+        self.x_vel = 2
+
+    def right_key_released(self, evt):
+        self.x_vel = 0
+
+
+refresh_rate = 0.01
+num_enemies = 16
+x_spacing = 40
+y_spacing = 40
+enemies = []
+heroes = []
+start_x = 10
+start_y = 10
+x = start_x
+y = start_y
+x_vel = 1.5
+y_vel = 0
+
+for i in range(num_enemies):
+
+    enemy = Enemy(canvas, x=x, y=y, x_vel=x_vel)
+    enemies.append(enemy)
+    x += x_spacing
+    if i == (num_enemies/2)-1:
+        x = start_x
+        y += y_spacing
+
+hero = Hero(canvas)
+heroes.append(hero)
+
+
+def draw_all_enemies():
+    for enemy in enemies:
+        enemy.draw()
+
+
+def wall_hit_check():
+    wall_hit = False
+    for enemy in enemies:
+        if enemy.hit_wall():
+            wall_hit = True
+    return wall_hit
+
+
+def shift_enemies():
+    for enemy in enemies:
+        enemy.x_vel = enemy.x_vel * -1
+        enemy.canvas.move(enemy.id, 0, 10)
+
+
+def draw_all_heroes():
+    for hero in heroes:
+        hero.draw()
+
+
+
+
+while 1:
+    draw_all_enemies()
+    draw_all_heroes()
+    enemy_hit_wall = wall_hit_check()
+    if enemy_hit_wall:
+        shift_enemies()
+
+
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(refresh_rate)
+
+
+tk.mainloop()
+

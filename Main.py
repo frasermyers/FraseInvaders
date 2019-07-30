10d6b87e44cbb069d3e83416e506ef009747ac84
Fraser <frasermyersmusic@gmail.com>
2019-07-30 18:24:54 +1000

    Version 1

--- Main.py
+++ Main.py
@@ -1,14 +1,135 @@
 from tkinter import *
 import time
+import winsound
+
+#TODO: Make highscore retain even when window is closed
+#TODO: Make mothership go across screen  every x number of bullets
+#TODO: End game when enemies hit the bottom
+#TODO: Enemies fire back
+
+winsound.PlaySound('arcade.wav', winsound.SND_LOOP + winsound.SND_ASYNC + winsound.SND_NOSTOP)
+
+bullet = None
+score = 0
+high_score = 0
+hero = None
+heroes = []
+enemies = []
+main_button_text = "Start Game"
+
+running = False
+
+
+params = {"refresh_rate": 0.01, "num_enemies": 1, "x_spacing": 40, "y_spacing": 40,
+          "start_x": 273, "start_y": 20, "x_vel": 1.5, "y_vel": 0, "win_text": None}
+
+
+def set_variables():
+    global hero
+    global heroes
+    for hero in heroes:
+        hero.canvas.delete(hero.id)
+    hero = Hero(canvas)
+    heroes = []
+    global enemies
+    for enemy in enemies:
+        enemy.canvas.delete(enemy.id)
+    enemies = []
+    heroes.append(hero)
+    x = params["start_x"]
+    y = params["start_y"]
+
+    for i in range(params["num_enemies"]):
+
+        enemy = Enemy(canvas, x=x, y=y, x_vel=params["x_vel"])
+
+        enemies.append(enemy)
+        x += params["x_spacing"]
+        if i == (params["num_enemies"] / 2) - 1:
+            x = params["start_x"]
+            y += params["y_spacing"]
+
+
+def start_game(evt):
+    global win_text
+    canvas.delete(start_text)
+
+    global score
+    global high_score
+    if score >= high_score:
+        high_score_label['text'] = "High Score: " + str(score)
+        high_score = score
+    score = 0
+    score_label['text'] = "Score: " + str(score)
+
+    winsound.PlaySound('win.wav', winsound.SND_ASYNC)
+
+    set_variables()
+    game_loop()
+
+
+def game_loop():
+    global running
+    global main_button_text
+    running = True
+    play_button.config(text="Restart")
+    while 1 and running:
+
+        win()
+        global bullet
+        if hit_check():
+            canvas.delete(bullet.id)
+            bullet = None
+        draw_all_enemies()
+        draw_all_heroes()
+        if bullet is not None:
+            bullet.draw()
+            if bullet.hit_roof():
+                bullet = None
+        enemy_hit_wall = wall_hit_check()
+        if enemy_hit_wall:
+            shift_enemies()
+
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(params["refresh_rate"])
+
+
+def pause(evt):
+    global running
+
+    if not running:
+        running = not running
+        pause_button.config(text="Pause Game")
+        game_loop()
+
+    else:
+        running = not running
+        pause_button.config(text="Resume Game")
+
 
 tk = Tk()
 tk.title("Frase Invaders!")
 tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
+score_label = Label(text="Score: " + str(score), bg='lightblue', font=('System', 18))
+score_label.grid(row=0, column=1, sticky='ew')
+high_score_label = Label(text="High score: " + str(high_score), bg='pink', font=('System', 18))
+high_score_label.grid(row=0, column=2, sticky='ew')
+play_button = Button(tk, text=main_button_text, bg="pink", font=('System', 18))
+play_button.bind("<Button-1>", start_game)
+play_button.grid(row=1, column=0, sticky='nesw')
+restart_button = Button(tk, text="Button does nothing", bg="pale green", font=('System', 18))
+restart_button.bind("<Button-1>")
+restart_button.grid(row=2, column=0, sticky='nesw')
+pause_button = Button(tk, text="Pause Game", bg="lightblue", font=('System', 18))
+pause_button.bind("<Button-1>", pause)
+pause_button.grid(row=3, column=0, sticky='nesw')
 canvas = Canvas(tk, width=720, height=560, bd=0, highlightthickness=0)
 canvas.config(bg="black")
-canvas.create_line(0, 30, 720, 30, fill="pink")
-canvas.pack()
+canvas.grid(row=1, column=1, rowspan=3, columnspan=2)
+start_text = canvas.create_text(360, 280, text="FRASE INVADERS", font=("System", 40), fill="white")
+win_text = None
 tk.update()
 
 
@@ -70,13 +191,13 @@ class Sprite:
 
 class Enemy(Sprite):
 
-    def __init__(self, canvas, color="Red", x=10, y=10, w=15, h=15, x_vel=1, y_vel=0):
+    def __init__(self, canvas, color="firebrick2", x=10, y=10, w=15, h=15, x_vel=1, y_vel=0):
         super(Enemy, self).__init__(canvas, color=color , x=x, y=y, w=w, h=h, x_vel=x_vel, y_vel=y_vel)
 
 
 class Hero(Sprite):
 
-    def __init__(self, canvas, color="SeaGreen1", x=360, y=500, w=40, h=40, x_vel=0, y_vel=0):
+    def __init__(self, canvas, color="deepskyblue2", x=340, y=500, w=40, h=40, x_vel=0, y_vel=0):
 
         super(Hero, self).__init__(canvas, color=color, x=x, y=y, w=w, h=h, x_vel=x_vel, y_vel=y_vel)
         self.canvas.bind_all('<KeyPress-Left>', self.left_key_pressed)
@@ -99,38 +220,9 @@ class Hero(Sprite):
 
 class Bullet(Sprite):
 
-    def __init__(self, canvas, color="Pink", x=360, y=400, w=6, h=10, x_vel=0, y_vel=-5, firing_object=None):
+    def __init__(self, canvas, color="lightblue", x=360, y=400, w=6, h=10, x_vel=0, y_vel=-5, firing_object=None):
         positions = firing_object.canvas.coords(firing_object.id)
         super(Bullet, self).__init__(canvas, color=color, x=positions[0], y=positions[1], w=w, h=h, x_vel=x_vel, y_vel=y_vel)
-
-
-refresh_rate = 0.01
-num_enemies = 10
-x_spacing = 40
-y_spacing = 40
-enemies = []
-heroes = []
-start_x = 10
-start_y = 40
-x = start_x
-y = start_y
-x_vel = 1.5
-y_vel = 0
-
-for i in range(num_enemies):
-
-    enemy = Enemy(canvas, x=x, y=y, x_vel=x_vel)
-    enemies.append(enemy)
-    x += x_spacing
-    if i == (num_enemies/2)-1:
-        x = start_x
-        y += y_spacing
-
-
-hero = Hero(canvas)
-heroes.append(hero)
-
-score = 0
 
 
 def draw_all_enemies():
@@ -160,58 +252,38 @@ def draw_all_heroes():
 def up_key_press(evt):
     global bullet
     if bullet is None:
+        global hero
         bullet = Bullet(canvas, firing_object=hero)
+        winsound.PlaySound('gunshot.wav', winsound.SND_ASYNC)
 
 
-score_text_1 = score_text = canvas.create_text(630, 15, text="Score: ", font=("Arial", 16), fill="white")
-score_text = canvas.create_text(680, 15, text=score, font=("Arial", 16), fill="white")
-
-
-def hit_check(bullet):
+def hit_check():
+    global bullet
     for enemy in enemies:
         if bullet is not None:
             if bullet.hit_other_object(enemy):
                 enemies.remove(enemy)
                 canvas.delete(enemy.id)
                 global score
-                global score_text
                 score += 1
-                canvas.delete(score_text)
-                score_text = canvas.create_text(680, 15, text=score, font=("Arial", 16), fill="white")
+                score_label['text'] = "Score: " + str(score)
+                winsound.PlaySound('hit.wav', winsound.SND_ASYNC)
+
                 return True
 
 
-win_text = None
-
-
 def win():
-    if score == num_enemies:
-        global win_text
-        win_text = canvas.create_text(360, 280, text="You Win!", font=("Arial", 40), fill="white")
+    global score
+    global high_score
+    global win_text
+    if score == params["num_enemies"]:
+        win_text = canvas.create_text(360, 280, text="You Win!", font=("System", 40), fill="white")
+    if score >= high_score:
+        high_score_label['text'] = "High Score: " + str(score)
+        high_score = score
 
 
 canvas.bind_all('<KeyPress-Up>', up_key_press)
-
-bullet = None
-
-
-while 1:
-    win()
-    hit_check(bullet)
-    draw_all_enemies()
-    draw_all_heroes()
-    if bullet is not None:
-        bullet.draw()
-        if bullet.hit_roof():
-            bullet = None
-    enemy_hit_wall = wall_hit_check()
-    if enemy_hit_wall:
-        shift_enemies()
-
-    tk.update_idletasks()
-    tk.update()
-    time.sleep(refresh_rate)
-
 
 tk.mainloop()
 
